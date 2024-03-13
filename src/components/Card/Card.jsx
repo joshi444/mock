39c5ea0441a3cc70d747ac85aaecbdf7311c33d1
joshi444 <joshimucheti86@gmail.com>
2024-03-13 18:01:55 +0530

    added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Card.css";
+import axios from "axios";
 
 function Card() {
+    const [data, setData] = useState([]);
 
-    const handleClick = () => {
-        window.open("https://www.w3schools.com/ai/ai_whatis.asp")
-    }
+    const handleClick = (url) => {
+        window.open(url);
+    };
 
+    useEffect(() => {
+        axios.get("https://run.mocky.io/v3/cb39edad-e67f-43f2-8e37-221251a7ea62")
+            .then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
-    return(
+    return (
         <div className="card">
-              <div className="card-body">
-                <p>ARTIFICIAL INTELLIGENCE</p>
-                <h1>Explore AI Solutions</h1>
-                <p>Discover how ai can transfrom your business operations and drive 
-                    innovation and enhancing the human experiences
-                </p>
-                <button className="card-button" onClick={handleClick}>Learn More</button>
-              </div>
+            {data.map((card, index) => (
+                <div className="card-body" key={index}>
+                    <p>{card.eye}</p>
+                    <h1>{card.head}</h1>
+                    <p>{card.desc}</p>
+                    <button className="card-button" onClick={() => handleClick(card.link)}>
+                        Learn More
+                    </button>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default Card;
